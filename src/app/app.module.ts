16d6f06c1066d85bdb6e19519297a7301ae9b7c5
Mateusz Konopka <mateusz.konopka11@gmail.com>
2022-12-07 14:02:39 +0100

    CRUD implemented and app connected with Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { TodoComponent } from './todo/todo.component';
+
+import {AngularFireModule} from '@angular/fire/compat'
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'todoapp-507ff',
+    appId: '1:337993040073:web:3f1b549216c7d8302c0d18',
+    storageBucket: 'todoapp-507ff.appspot.com',
+    apiKey: 'AIzaSyCkc-dT_3Qt_YqM1nbQjkA7uTZVEo1wXtQ',
+    authDomain: 'todoapp-507ff.firebaseapp.com',
+    messagingSenderId: '337993040073',
+  },
   production: false
 };
 
